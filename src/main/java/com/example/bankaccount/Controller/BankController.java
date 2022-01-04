a1fdc1ea6cfd52c090b6f82dfa143dd03f7a49b2
archanvel <disk.space456@gmail.com>
2022-01-04 19:57:40 +0200

    Initial commit

---
+++ src/main/java/com/example/bankaccount/Controller/BankController.java
@@ -0,0 +1,31 @@
+package com.example.bankaccount.Controller;
+
+import com.example.bankaccount.Main.Bank;
+import com.example.bankaccount.Service.BankService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class BankController {
+    @Autowired
+    private BankService service;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        List<Bank> listbank = service.listAll();
+        model.addAttribute("listbank", listbank);
+        System.out.print("Get / ");
+        return "bank";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveBank(@ModelAttribute("bank") Bank bank) {
+        service.save(bank);
+        return "redirect:/";
+    }
+}
